1562c3f203256c009b43641ffc5b68592c4b484d
Yanis Guenane <yguenane@gmail.com>
2014-07-13 21:08:18 -0400

    postgresql: Offer more lib to install

--- manifests/database/postgresql.pp
+++ manifests/database/postgresql.pp
@@ -6,14 +6,48 @@
 #   * yguenane/postgresqlrepo
 #   * puppetlabs/postgresql
 #
-class profile::database::postgresql {
+class profile::database::postgresql (
+  $contrib_enable = true,
+  $devel_enable   = false,
+  $postgis_enable = false,
+  $java_enable    = false,
+  $perl_enable    = false,
+  $python_enable  = false,
+  $plperl_enable  = false,
+) {
+
   include profile::base
 
   include ::postgresqlrepo
   include ::postgresql::globals
   include ::postgresql::server
-  include ::postgresql::server::contrib
-  include ::postgresql::server::plperl
-  include ::postgresql::lib::java
-  include ::postgresql::lib::python
+
+  if $contrib_enable {
+    include ::postgresql::server::contrib
+  }
+
+  if $devel_enable {
+    include ::postgresql::server::devel
+  }
+
+  if $postgis_enable {
+    include ::postgresql::server::postgis
+  }
+
+  if $plperl_enable {
+    include ::postgresql::server::plperl
+  }
+
+  if $perl_enable {
+    include ::postgresql::lib::perl
+  }
+
+  if $java_enable {
+    include ::postgresql::lib::java
+  }
+
+  if $python_enable {
+    include ::postgresql::lib::python
+  }
+
 }
