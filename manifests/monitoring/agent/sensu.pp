7f4a6cb195617b69386b7c42e818309ab4191488
Yanis Guenane <yguenane@gmail.com>
2014-07-31 10:09:08 -0400

    monitoring/agent/sensu: Allow to install standalone checks

--- manifests/monitoring/agent/sensu.pp
+++ manifests/monitoring/agent/sensu.pp
@@ -5,6 +5,12 @@
 # Puppet module :
 #   mod 'sensu/sensu'
 #
-class profile::monitoring::agent::sensu {
+class profile::monitoring::agent::sensu (
+  $plugins = {},
+  $checks  = {},
+) {
   include ::sensu
+
+  create_resources('::sensu::plugin', $plugins)
+  create_resources('::sensu::check', $checks)
 }
