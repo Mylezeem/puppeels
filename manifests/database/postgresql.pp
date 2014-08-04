86bbcbe71f92fab9105a8193d0e9289e1c7e0cdc
Yanis Guenane <yguenane@gmail.com>
2014-08-04 09:08:30 -0400

    Postgresql: Add wrapper around resources

--- manifests/database/postgresql.pp
+++ manifests/database/postgresql.pp
@@ -7,13 +7,21 @@
 #   * puppetlabs/postgresql
 #
 class profile::database::postgresql (
-  $contrib_enable = true,
-  $devel_enable   = false,
-  $postgis_enable = false,
-  $java_enable    = false,
-  $perl_enable    = false,
-  $python_enable  = false,
-  $plperl_enable  = false,
+  $contrib_enable  = true,
+  $devel_enable    = false,
+  $postgis_enable  = false,
+  $java_enable     = false,
+  $perl_enable     = false,
+  $python_enable   = false,
+  $plperl_enable   = false,
+  $config_entries  = {},
+  $dbs             = {},
+  $databases       = {},
+  $database_grants = {},
+  $pg_hba_rules    = {},
+  $roles           = {},
+  $table_grants    = {},
+  $tablespaces     = {},
 ) {
 
   include profile::base
@@ -49,5 +57,14 @@ class profile::database::postgresql (
   if $python_enable {
     include ::postgresql::lib::python
   }
+
+  create_resources('postgresql::server::config_entry', $config_entries)
+  create_resources('postgresql::server::db', $dbs)
+  create_resources('postgresql::server::database', $databases)
+  create_resources('postgresql::server::database_grant', $database_grants)
+  create_resources('postgresql::server::pg_hba_rule', $pg_hba_rules)
+  create_resources('postgresql::server::role', $roles)
+  create_resources('postgresql::server::table_grant', $table_grants)
+  create_resources('postgresql::server::tablespace', $tablespaces)
 
 }
