99773f35e58e376e893f54bedd8e92ac35d8103c
Yanis Guenane <yguenane@gmail.com>
2014-08-22 20:46:20 -0400

    application/mediawiki.pp: Update to reflect current work on puppet-mediawiki

--- manifests/application/mediawiki.pp
+++ manifests/application/mediawiki.pp
@@ -3,36 +3,15 @@
 # License: ApacheV2
 #
 # Puppet module :
+#   mod 'mediawiki',
+#     :git => 'https://github.com/Mylezeem/puppet-mediawiki.git',
+#     :ref => 'master'
 #
 class profile::application::mediawiki (
-  $mediawiki_vhost  = {},
+  $wikis  = {},
 ) {
 
   include profile::base
-  include profile::database::mariadb
-  include profile::webserver::apache
-  create_resources('apache::vhost', $mediawiki_vhost)
-
-  mysql_database {'mediawiki' :
-    ensure  => 'present',
-    charset => 'utf8',
-    collate => 'utf8_swedish_ci',
-    require => Service['mysql'],
-  } ->
-  mysql_user { 'mediawiki@127.0.0.1' :
-    ensure => present,
-  } ->
-  mysql_grant { 'mediawiki@127.0.0.1/mediawiki.*' :
-    ensure     => present,
-    options    => ['GRANT'],
-    privileges => ['ALL'],
-    table      => 'mediawiki.*',
-    user       => 'mediawiki@127.0.0.1',
-  }
-
-  package { 'mediawiki119' :
-    ensure  => present,
-    require => Class['epel'],
-  }
+  create_resources('mediawiki::wiki', $wikis)
 
 }
