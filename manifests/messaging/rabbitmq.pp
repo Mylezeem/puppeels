c52e72ec07a92b195200a35cd931f5743803dd21
Yanis Guenane <yguenane@gmail.com>
2014-09-09 14:34:50 -0400

    messaging/rabbitmq.pp: Initial Commit

---
+++ manifests/messaging/rabbitmq.pp
@@ -0,0 +1,25 @@
+#
+# Author: Yanis Guenane <yguenane@gmail.com>
+# License: ApacheV2
+#
+# Puppet module :
+#   * puppetlabs/rabbitmq
+#
+class profile::messaging::rabbitmq (
+  $users            = {},
+  $vhosts           = {},
+  $exchanges        = {},
+  $user_permissions = {},
+  $plugins          = {},
+) {
+
+  include profile::base
+
+  include ::rabbitmq
+  create_resources('rabbitmq_user', $users)
+  create_resources('rabbitmq_vhost', $vhosts)
+  create_resources('rabbitmq_exchange', $exchanges)
+  create_resources('rabbitmq_user_permissions', $user_permissions)
+  create_resources('rabbitmq_plugin', $plugins)
+
+}
