7271008fc6e97d04047970c9241db63bdcf85991
Yanis Guenane <yguenane@gmail.com>
2014-09-10 14:06:32 -0400

    application/puppet_master: Use the inifile module to configure puppet master

--- manifests/application/puppet_master.pp
+++ manifests/application/puppet_master.pp
@@ -5,17 +5,29 @@
 # Puppet module :
 #   * puppetlabs/puppetdb
 #   * zack/r10k
+#   * puppetlabs/inifile
 #
 class profile::application::puppet_master (
-  $puppetdb_enable = false,
+  $puppetconf_path      = '/etc/puppet/puppet.conf',
+  $puppetdb_enable      = false,
+  $r10k_enable          = false,
+  $main_configuration   = {},
+  $agent_configuration  = {},
+  $master_configuration = {},
 ) {
+
   include profile::base
 
-  include ::puppet
-  include ::r10k
+  if $r10k_enable {
+    include ::r10k
+  }
 
   if $puppetdb_enable {
     include ::puppetdb::master::config
   }
+
+  create_resources('ini_setting', $main_configuration, { 'section' => 'main', 'path' => $puppetconf_path })
+  create_resources('ini_setting', $agent_configuration, { 'section' => 'agent', 'path' => $puppetconf_path })
+  create_resources('ini_setting', $master_configuration, { 'section' => 'master', 'path' => $puppetconf_path })
 
 }
