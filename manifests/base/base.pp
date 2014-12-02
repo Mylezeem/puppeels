643ed180dc663a00eeec576aeff32839c33fc9ae
Michael Chapman <woppin@gmail.com>
2014-12-03 00:06:45 +1100

    base: Add firewall and augeas options

--- manifests/base/base.pp
+++ manifests/base/base.pp
@@ -8,20 +8,25 @@
 #   mod 'puppetlabs/ntp'
 #
 class profile::base::base (
-  $manage_epel       = true,
-  $manage_accounts   = true,
-  $manage_logging    = 'fluentd',
-  $manage_monitoring = 'sensu',
-  $manage_sshd       = true,
-  $manage_ntp        = true,
-  $manage_sudo       = true,
-  $manage_authconfig = true,
-  $base_packages     = true,
-  $package_ensure    = 'installed',
-  $base_classes      = [],
+  $manage_augeasproviders  = true,
+  $manage_epel             = true,
+  $manage_accounts         = true,
+  $manage_logging          = 'fluentd',
+  $manage_monitoring       = 'sensu',
+  $manage_sshd             = true,
+  $manage_ntp              = true,
+  $manage_sudo             = true,
+  $manage_authconfig       = true,
+  $manage_firewall         = true,
+  $base_packages           = [],
+  $package_ensure          = 'installed',
+  $base_classes            = [],
 ) {
-  include ::augeasproviders
   include $base_classes
+
+  if $manage_augeasproviders {
+    include ::augeasproviders
+  }
 
   if $manage_accounts {
     include profile::base::accounts
@@ -54,6 +59,10 @@ class profile::base::base (
 
   if $manage_authconfig {
     include ::authconfig
+  }
+
+  if $manage_firewall {
+    include ::firewall
   }
 
   if $base_packages {
