e322d7bc6ac23776c47a17329733e18010288703
Michael Chapman <woppin@gmail.com>
2014-12-04 01:14:24 +1100

    openstack: Improvements to compute/hypervisor
    Check hypervisor choice is valid, make telemetry
    optional. Add nova::compute::neutron.

--- manifests/openstack/compute/hypervisor.pp
+++ manifests/openstack/compute/hypervisor.pp
@@ -1,11 +1,21 @@
 class profile::openstack::compute::hypervisor (
   $hypervisor_type = 'libvirt', # Possible value libvirt, vmware, xenserver
+  $manage_telemetry = true
 ) {
   include ::profile::openstack::compute
-  include ::profile::openstack::telemetry
   include ::profile::openstack::network
   include ::nova::compute
+  include ::nova::compute::neutron
   include ::nova::compute::spice
-  include ::nova::compute::${hypervisor_type}
-  include ::ceilometer::agent::compute
+
+  if $hypervisor_type in ['libvirt', 'vmware', 'xenserver'] {
+    include "::nova::compute::${hypervisor_type}"
+  } else {
+    fail("Invalid hypervisor_type selected: ${hypervisor_type}")
+  }
+
+  if $manage_telemetry {
+    include ::profile::openstack::telemetry
+    include ::ceilometer::agent::compute
+  }
 }
