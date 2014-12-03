437461869c21609c77267f50236188a784eb2cb7
Michael Chapman <woppin@gmail.com>
2014-12-04 01:16:01 +1100

    openstack: add firewall to neutron-server

--- manifests/openstack/network/controller.pp
+++ manifests/openstack/network/controller.pp
@@ -1,6 +1,15 @@
-class profile::openstack::network::controller {
+class profile::openstack::network::controller(
+  $manage_firewall = true,
+  $firewall_extras = {}
+) {
   include profile::openstack::network
 
   include ::neutron::server
   include ::neutron::server::notifications
+  if $manage_firewall {
+    profile::firewall::rule { '210 neutron-server accept tcp':
+      port   => 9696,
+      extras => $extras
+    }
+  }
 }
