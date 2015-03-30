993b705863626cec86e34c2dabb8ae691bffc5ad
Jan Ivar Beddari <code@beddari.net>
2015-03-30 14:35:07 +0200

    Added eyaml for Puppet/Foreman

--- manifests/application/foreman.pp
+++ manifests/application/foreman.pp
@@ -8,12 +8,19 @@
 #   mod 'theforeman/puppet'
 #   mod 'theforeman/tftp'
 #   mod 'zack/r10k'
+#   mod 'ghoneycutt/eyaml
 #
-class profile::application::foreman {
+class profile::application::foreman(
+  $manage_eyaml = false
+) {
 
   include ::r10k
   include ::tftp
   include ::puppet
   include ::foreman
   include ::foreman_proxy
+
+  if $manage_eyaml {
+    include ::eyaml
+  }
 }
