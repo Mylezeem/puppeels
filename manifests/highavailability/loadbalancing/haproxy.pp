f7569c01695ec004bb3190b3a467505020fcefc4
Yanis Guenane <yanis@guenane.org>
2015-04-12 23:24:28 +0200

    HAProxy: Add support for more defined type

--- manifests/highavailability/loadbalancing/haproxy.pp
+++ manifests/highavailability/loadbalancing/haproxy.pp
@@ -1,5 +1,5 @@
 #
-# Author: Yanis Guenane <yguenane@gmail.com>
+# Author: Yanis Guenane <yanis@guenane.org>
 # License: ApacheV2
 #
 # Puppet module :
@@ -8,12 +8,21 @@
 #   mod 'puppetlabs/concat'
 #
 class profile::highavailability::loadbalancing::haproxy (
-  $haproxy_listens           = {},
-  $haproxy_balancermembers   = {},
+  $haproxy_listens         = {},
+  $haproxy_frontends       = {},
+  $haproxy_backends        = {},
+  $haproxy_balancermembers = {},
+  $haproxy_userlists       = {},
+  $haproxy_peers           = {},
 ) {
 
   include ::haproxy
+
   create_resources('haproxy::listen', $haproxy_listens)
+  create_resources('haproxy::backend', $haproxy_backends)
+  create_resources('haproxy::frontend', $haproxy_frontends)
   create_resources('haproxy::balancermember', $haproxy_balancermembers)
+  create_resources('haproxy::userlist', $haproxy_userlists)
+  create_resources('haproxy::peer', $haproxy_peers)
 
 }
